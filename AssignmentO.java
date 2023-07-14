8c6982e9f684947b70581ba629f390edb13ddd2e
Anuj-c0d3 <amvj111tbpl.com>
2023-07-14 15:12:35 +0530

    Assignment added to new branch

---
+++ AssignmentO.java
@@ -0,0 +1,29 @@
+public class AssignmentO {
+    public static void main(String[] args) {
+        // assignment
+        int a = 10;
+        System.out.println(a);
+        int m, b, c, d;
+        m = 10;
+        b = 11;
+        c = 20;
+        d = 90;
+        System.out.println("m,b,c,d");
+        System.out.println(m + " " + b + " " + c + " " + d);
+        // chaining method of assigning
+        int k, l, n, p;
+        k = l = n = p = 70;
+        System.out.println("k,l,n,p");
+        System.out.println(k + " " + l + " " + n + " " + p);
+        int x = 10;
+        System.out.println("x=" + x);
+        x = x + 5;
+        System.out.println("x=" + x);
+        // we can do this by following assignment method
+        int y = 10;
+        System.out.println("y=" + y);
+        y += 5;
+        System.out.println("y=" + y);
+
+    }
+}
